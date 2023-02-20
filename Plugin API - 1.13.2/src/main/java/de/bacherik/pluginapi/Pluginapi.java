fade5318b090c9fc29cbfa0277f95c274d5f3d4a
BachErik <bacherik.info@gmail.com>
2023-02-20 19:31:13 +0100

    Add 1.13.2

---
+++ Plugin API - 1.13.2/src/main/java/de/bacherik/pluginapi/Pluginapi.java
@@ -0,0 +1,17 @@
+package de.bacherik.pluginapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pluginapi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
