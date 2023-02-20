aeb9ad4cac36437114bf90fb67bebd829987cc72
BachErik <75324423+BachErik@users.noreply.github.com>
2023-02-20 20:40:19 +0100

    Create build_all_plugins.sh

---
+++ build_all_plugins.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+OUTPUT_DIR=output
+
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir "$OUTPUT_DIR"
+fi
+
+for dir in */; do
+  if [ -e "$dir/pom.xml" ]; then
+    echo "Building Maven project in directory $dir"
+    cd "$dir" && mvn clean package && cd ..
+    cp "$dir/target/"*.jar "$OUTPUT_DIR/"
+  fi
+done
