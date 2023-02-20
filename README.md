563eb7b4485106295421f712a1b76ec84c46fbae
BachErik <bacherik.info@gmail.com>
2023-02-20 19:10:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Plugin-API
+ This is a API that should make it easier to create Spigot plugins.
---
+++ build_all_plugins.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set OUTPUT_DIR=output
+
+if not exist %OUTPUT_DIR% mkdir %OUTPUT_DIR%
+
+for /d %%i in (*) do (
+    if exist "%%i\pom.xml" (
+        echo Building Maven project in directory %%i
+        pushd "%%i" && mvn clean package && popd
+        copy "%%i\target\*.jar" "%OUTPUT_DIR%\"
+    )
+)
