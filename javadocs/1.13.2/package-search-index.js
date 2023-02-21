2ecdf72988940b5fe3cdafa28d90d9760b5b34f3
BachErik <bacherik.info@gmail.com>
2023-02-21 20:22:53 +0100

    remove javadoc

--- javadocs/1.13.2/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.bacherik.pluginapi"},{"l":"de.bacherik.pluginapi.builder"}];updateSearchResults();
--- javadocs/1.13.2/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"de.bacherik.pluginapi.builder","l":"BossBarBuilder"},{"p":"de.bacherik.pluginapi","l":"Main"}];updateSearchResults();
